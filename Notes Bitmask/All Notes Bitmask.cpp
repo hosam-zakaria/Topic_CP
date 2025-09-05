b5ff8e84d065d9b5c267fca550fc8e6faface50c
Hosam Zakaria <hossamzakaria61@gmail.com>
2025-09-06 00:49:16 +0300

    All Notes Bitmask.cpp

---
+++ Notes Bitmask/All Notes Bitmask.cpp
@@ -0,0 +1,11 @@
+// return count number one in Binary number 
+__builtin_popcount(int number);
+
+// return count number of Zero from rigth direction binary number 
+__builtin_ctz(x)
+// ex: 001100  => __builtin_ctz(001100) = 2 
+
+// return count number of Zero from lift direction binary number 
+__builtin_clz(x)
+// ex: 001100  => __builtin_ctz(0001100) = 3 
+
