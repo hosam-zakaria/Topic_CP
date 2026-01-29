78977059e6daaf74681783fbb01c56c4aa938646
Hosam Zakaria <hossamzakaria61@gmail.com>
2026-01-29 12:05:29 +0200

    Add Sieve function for prime number detection
    Implement Sieve of Eratosthenes to check for prime numbers.

--- Notes Math/Function sieve.cpp
+++ Notes Math/Function sieve.cpp
@@ -1,4 +1,9 @@
 // sieve => chack number prime or not O(n log log n)
+
+const int mx = 100005;  
+bool prime[mx + 1]; 
+vector<ll> pre(mx); 
+
 void sieve() {
     fill(prime, prime + mx + 1, true);
     prime[0] = prime[1] = false; // 0 1 Not Prime 
