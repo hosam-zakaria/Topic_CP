2ee6e7fe462d6c1e3f13b323677e458443efe059
Hosam Zakaria <hossamzakaria61@gmail.com>
2026-07-10 22:25:06 +0300

    Create Summation.cpp

---
+++ Math Tracks /Summation.cpp
@@ -0,0 +1,19 @@
+// Sum 1 -> n
+n * (n + 1) / 2
+
+// Sum l -> r
+r * (r + 1) / 2 - (l - 1) * l / 2
+
+// Sum Even
+even = n / 2;
+even * (even + 1);
+
+// Sum Odd
+odd = n - n / 2;
+odd * odd;
+
+// Sum Squares
+n * (n + 1) * (2 * n + 1) / 6
+
+// Sum Cubes
+(n * (n + 1) / 2)^2
