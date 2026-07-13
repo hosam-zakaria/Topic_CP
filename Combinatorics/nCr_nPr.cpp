e6aa7ed4732f320ec193b5b59a1b31bd49f0580a
Hosam Zakaria <hossamzakaria61@gmail.com>
2026-07-13 15:28:52 +0300

    Update nCr_nPr.cpp

--- Combinatorics/nCr_nPr.cpp
+++ Combinatorics/nCr_nPr.cpp
@@ -21,3 +21,30 @@ ll mod_inv(int x) { return expo_power(x,MOD-2)%MOD ; }
 ll mod_div(ll a, ll b) { a %= MOD; return (a * mod_inv(b)) % MOD; }
 ll nPr(int n ,int r) { return fact[n]*mod_inv(fact[n-r])%MOD; }
 ll nCr(int n ,int r) { return fact[n]*mod_inv(fact[r]*fact[n-r]%MOD)%MOD; }
+
+
+----------------------------------------------------------------------------
+ll fact[N], invfact[N];
+
+void calc_fact()
+{
+    fact[0] = 1;
+    for(int i = 1; i < N; i++)
+        fact[i] = fact[i-1] * i % MOD;
+
+    invfact[N-1] = 1;
+    ll x = fact[N-1], p = MOD - 2;
+    while(p){
+        if(p & 1)
+            invfact[N-1] = invfact[N-1] * x % MOD;
+        x = x * x % MOD;
+        p >>= 1;
+    }
+    for(ll i = N - 2; i >= 0; i--){
+        invfact[i] = invfact[i+1] * (i+1) % MOD;
+    }
+}
+ll nCr(ll n, ll r){
+    if(r < 0 || r > n) return 0;
+    return fact[n] * invfact[r] % MOD * invfact[n-r] % MOD;
+}
