9d9f98536c26608b85a3807a1f3e2b75c3351df5
Hosam Zakaria <hossamzakaria61@gmail.com>
2026-07-16 20:46:09 +0300

    Implement DFS for calculating node distances
    Added a DFS function to calculate distances from a node.

--- Graph theory/tree diameter.cpp
+++ Graph theory/tree diameter.cpp
@@ -6,3 +6,15 @@ pair<int, int> dfs0(int node, int par, int cnt)
         ret = max(ret, dfs0(child, node, cnt+1));
     return ret;
 }
+
+--------------------------------------------------
+// dist Node 
+ll distA[N + 1], distB[N + 1];
+void dfs(ll node, ll par, ll d, ll dist[])
+{
+    dist[node] = d;
+    for(auto child : adj[node])
+        if(child != par)
+            dfs(child, node, d + 1, dist);
+}
+    
