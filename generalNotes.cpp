7ce1c85aee7140cc248d0ef0c0340db0ac950c1f
Hosam Zakaria <hossamzakaria61@gmail.com>
2026-07-27 20:19:46 +0300

    Create generalNotes.cpp

---
+++ generalNotes.cpp
@@ -0,0 +1,12 @@
+/*
+(i * s) % n == 0
+i == The first multiple of s divisible by n ==> LCM(n, s) = i * s
+LCM(n, s) / s == (n * s) / (GCD(n, s) * s)
+LCM(n, s) / s == (n / GCD(n, s))
+cout << lcm(n, s) / s << endl; 
+or 
+cout << n / gcd(n, s)  << endl; 
+*/
+
+//----------------------------------------------------
+//----------------------------------------------------
